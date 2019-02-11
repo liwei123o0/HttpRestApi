c6312ae37bf6bfd48906c56e9420fe1038e68c07
Administrator <877129310@qq.com>
2019-02-11 16:36:20 +0800

    南京V3.0

--- httpapi/app/views.py
+++ httpapi/app/views.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 # ! /usr/bin/env python
-from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse
+from datetime import datetime
+
 import pysolr
+from django.http import JsonResponse
 from httpapi.settings import Solr_URL
+
 from .models import *
+
+
 # from rest_framework.permissions import IsAuthenticated
 # from rest_framework.decorators import api_view, permission_classes
 
@@ -15,17 +19,26 @@ def analysis_solr_api(request):
     area = request.GET.get('area', 0)
     credit_quality = request.GET.get('quality', 0)
     rows = request.GET.get('rows', 10)
-    if area == 0 or credit_quality == 0 or rows == 0:
+    quarter = request.GET.get('quarter', "0")
+    quarters = {"1": "[%s-01-01T00:00:00Z TO %s-03-31T00:00:00Z]" % (datetime.now().year, datetime.now().year),
+                "2": "[%s-04-01T00:00:00Z TO %s-06-30T00:00:00Z]" % (datetime.now().year, datetime.now().year),
+                "3": "[%s-07-01T00:00:00Z TO %s-09-30T00:00:00Z]" % (datetime.now().year, datetime.now().year),
+                "4": "[%s-10-01T00:00:00Z TO %s-12-31T00:00:00Z]" % (datetime.now().year, datetime.now().year),
+                "0": "[%s-%s-01T00:00:00Z TO *]" % (datetime.now().year, datetime.now().month)}
+    if area == 0 or credit_quality == 0:
         return JsonResponse({"msg": "error", "data": u"请求参数未填写正确！"})
-    q_dict = {'wt': 'json', "rows": rows}
+    q_dict = {'wt': 'json', "rows": rows, "fq": "sortpubtime:%s" % quarters.get(quarter)}
+    print q_dict
     result = SolrReKeyWord.objects.filter(credit_quality_id=credit_quality, flag=1).first()
     area_re = AreaReKeyWord.objects.filter(area=area, flag=1).first().word_re
-    if result ==None or area_re ==None:
+    if result == None or area_re == None:
         return JsonResponse({"msg": "error", "data": u"指标参数或区域参数不正确！"})
     area_re = area_re + result.area_re
     word_re = result.word_re
-    re_search = "(" + area_re + "AND" + word_re + ")"
-    q = 'content:%s' % re_search
+    # re_search = "(" + area_re + "AND" + word_re + ")"
+    q = '((title:(('"%s"')AND('"%s"')))AND(content:(('"%s"')AND('"%s"'))))' % (area_re, word_re, area_re, word_re)
+    # q = 'content:%s' % re_search
+    # print q
     try:
         solr = pysolr.Solr(Solr_URL, timeout=10)
     except:
---
+++ httpapi/ceshi.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# ! /usr/bin/env python
+
+"""
+@author:LiWei
+@license:LiWei
+@contact:877129310@qq.com
+@version:
+@var:
+@note:
+
+"""
+from datetime import datetime
+
+print datetime.now().month
